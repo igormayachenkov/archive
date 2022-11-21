30199c6da51d4529d53a76fd8d8fa1d754ec0928
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2022-11-21 18:55:18 +0300

    config path hardcoded

--- config.js
+++ config.js
@@ -2,13 +2,15 @@ import fs from "fs-extra"
 
 export const config = {}
 
+const CONFIG_PATH = '/etc/archive/config.json'
+
 export function loadConfig(){
     // Get config filename
-    let configfile = process.argv[2]
-    if(!configfile) throw 'ERROR: configfile is not specified (the first process argument)'
+    //let configfile = process.argv[2]
+    //if(!configfile) throw 'ERROR: configfile is not specified (the first process argument)'
 
     // Read the file
-    let rawdata = fs.readFileSync(configfile);
+    let rawdata = fs.readFileSync(CONFIG_PATH);
     let json = JSON.parse(rawdata);
     Object.assign(config,json)
 
